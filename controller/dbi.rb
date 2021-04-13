b5cca91a2e97f9a3f33801ff6a0a12dc2486e582
JFLABO[JP] <vortex003@msn.com>
2021-04-13 22:38:31 +0900

    Update dbi.rb

--- controller/dbi.rb
+++ controller/dbi.rb
@@ -1,8 +1,10 @@
 require 'dbi'
-  # dsn = %q(DBI:ODBC:Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=c://db/Sample.accdb;)
-  dsn = %q(DBI:ODBC:Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=//127.0.0.1/db/Sample.accdb;)
+   dsn = %q(DBI:ODBC:Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=c://db/Sample.accdb;)
+  #sn = %q(DBI:ODBC:Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=//127.0.0.1/db/Sample.accdb;)
   DBI.connect(dsn)
-
+  # MySQLへの接続
+  # DBI.connect(DSN["dbi":drivername:databese], user, password)
+  #dbh = DBI.connect("dbi:Mysql:demo", "root", "mysql")
     sql = to_sjis("SELECT Piyo From Hoge WHERE Fuga = ?")
     bind = to_sjis(params[:hoge].to_s)
 
